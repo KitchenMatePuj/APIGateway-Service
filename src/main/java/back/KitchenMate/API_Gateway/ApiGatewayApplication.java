5ed33cb9ca941d46258e2a9f50c164d0b9c5d7b7
iTMG22 <tomashmartinez8@gmail.com>
2025-03-21 21:53:35 -0500

    First API Gateway Commit

---
+++ src/main/java/back/KitchenMate/API_Gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package back.KitchenMate.API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
