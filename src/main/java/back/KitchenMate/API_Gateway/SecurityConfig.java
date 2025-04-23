7bafce4684113daf622c1d92594335bc212bfa14
iTMG22 <tomashmartinez8@gmail.com>
2025-04-23 18:24:28 -0500

    Se Añade la funcionalidad de Validacion de JWT

---
+++ src/main/java/back/KitchenMate/API_Gateway/SecurityConfig.java
@@ -0,0 +1,32 @@
+package back.KitchenMate.API_Gateway;
+
+import org.springframework.beans.factory.annotation.Value; // Importar Value
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${AUTH_PATH}")
+    private String authPathPattern;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(authPathPattern).permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
